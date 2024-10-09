10132cb11bd8aa0e481636754c1d65f400e2eae5
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-10-09 13:57:07 +0200

    first commit

---
+++ src/main/Java/com/example/rgbLed/controller/LedController.java
@@ -0,0 +1,26 @@
+package com.example.rgbled.controller;
+
+import com.example.rgbled.service.LedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class LedController {
+
+    @Autowired
+    private LedService ledService;
+
+    @PostMapping("/set_color")
+    public String setLedColor(@RequestParam int r, @RequestParam int g, @RequestParam int b) {
+        ledService.setColor(r, g, b);
+        return "LED color set to RGB(" + r + ", " + g + ", " + b + ")";
+    }
+
+    @PostMapping("/cycle_colors")
+    public String cycleColors() {
+        ledService.cycleColors();
+        return "Cycling colors!";
+    }
+}
+
